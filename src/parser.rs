639503912e31393db52c42bd4634532e794ba55c
Scott Olson <scott@solson.me>
2016-03-03 13:09:34 -0600

    Add all Nix token kinds.

--- src/parser.rs
+++ src/parser.rs
@@ -1,26 +1,49 @@
-#[derive(Clone, Debug)]
+use symbol_table::{Symbol, SymbolTable};
+
+#[derive(Clone, Debug, PartialEq)]
 enum TokenKind {
+    // Basic
     Id(Symbol),
     Int(i64),
     Float(f64),
     Path(Symbol),
 
-    Bang, // !
-    At,   // @
-    Star, // *
-    Minus,
-    Plus,
-    Equals,
+    // String-related
+    Uri(String),
+    StrPart(String),
+    IndentStrPart(String),
+    Quote,          // "
+    IndentQuote,    // ''
+    DollarBrace,    // ${
 
-    Colon,
-    Semicolon,
-    Comma,
-    DollarBrace, // `${` can't be written as `$ {`
+    // Operators
+    Bang,       // !
+    Star,       // *
+    Minus,      // -
+    Plus,       // +
+    Divide,     // /
+    Assign,     // =
+    Equals,     // ==
+    NotEquals,  // !=
+    And,        // &&
+    Or,         // ||
+    Implies,    // ->
+    Concat,     // ++
 
-    ParenL,
-    ParenR,
-    BracketL,
-    BracketR,
-    BraceL,
-    BraceR,
+    // Other syntax
+    At,         // @
+    Comma,      // ,
+    Dot,        // .
+    Ellipsis,   // ...
+    Question,   // ?
+    Colon,      // :
+    Semicolon,  // ;
+
+    // Delimiters
+    ParenL,     // (
+    ParenR,     // )
+    BracketL,   // [
+    BracketR,   // ]
+    BraceL,     // {
+    BraceR,     // }
 }
