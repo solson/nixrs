80abfdd9780a9420d62a728920f934e744911322
Scott Olson <scott@solson.me>
2016-03-26 00:06:52 -0600

    Track character positions and add an int token test.

--- src/parse.rs
+++ src/parse.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::fmt;
 use std::str::Chars;
 
 use context::EvalContext;
@@ -83,9 +84,8 @@ pub enum TokenKind {
 pub struct Lexer<'ctx, 'src> {
     ectx: &'ctx EvalContext,
     source: &'src str,
-    chars: Chars<'src>,
+    chars: CharsPos<'src>,
     filename: Symbol,
-    pos: Pos,
 }
 
 impl<'ctx, 'src> Lexer<'ctx, 'src> {
@@ -93,24 +93,27 @@ impl<'ctx, 'src> Lexer<'ctx, 'src> {
         Lexer {
             ectx: ectx,
             source: source,
-            chars: source.chars(),
+            chars: CharsPos::new(source.chars()),
             filename: ectx.intern(filename),
-            pos: Pos { line: 1, column: 1 },
         }
     }
 
     fn lex_int(&mut self) -> Token {
-        let start = self.pos;
+        let start = self.pos();
         let chars = self.chars.as_str();
         let num_digits = self.chars.take_while_ref(|c| c.is_digit(10)).count();
         let digits = &chars[..num_digits];
 
         // TODO(tsion): Detect and diagnose integer overflow.
-        self.spanned(start, self.pos, TokenKind::Int(digits.parse::<i64>().unwrap()))
+        self.spanned(start, self.pos(), TokenKind::Int(digits.parse::<i64>().unwrap()))
     }
 
     fn peek(&self) -> Option<char> {
         self.chars.clone().next()
+    }
+
+    fn pos(&self) -> Pos {
+        self.chars.pos
     }
 
     fn spanned<T>(&self, start: Pos, end: Pos, val: T) -> Spanned<T> {
@@ -133,17 +136,70 @@ impl<'ctx, 'src> Iterator for Lexer<'ctx, 'src> {
     }
 }
 
+/// An iterator wrapping a `std::str::Chars` iterator which also keeps track of the current line
+/// and column position.
+#[derive(Clone)]
+struct CharsPos<'a> {
+    chars: Chars<'a>,
+    pos: Pos,
+}
+
+impl<'a> CharsPos<'a> {
+    fn new(chars: Chars<'a>) -> Self {
+        CharsPos { chars: chars, pos: Pos { line: 1, column: 1 } }
+    }
+
+    fn as_str(&self) -> &'a str {
+        self.chars.as_str()
+    }
+}
+
+impl<'a> Iterator for CharsPos<'a> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<char> {
+        let opt_c = self.chars.next();
+        match opt_c {
+            Some('\n') => { self.pos.line += 1; self.pos.column = 1; }
+            Some(_) => { self.pos.column += 1; }
+            None => {}
+        }
+        opt_c
+    }
+}
+
 pub fn lex(ectx: &EvalContext, filename: &str, source: &str) -> Vec<Token> {
     Lexer::new(ectx, filename, source).collect()
 }
 
+impl fmt::Display for Pos {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}", self.line, self.column)
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use super::lex;
+    use context::EvalContext;
+    use parse::{Lexer, TokenKind};
+
+    macro_rules! assert_lex {
+        ($src:expr => [ $($span:expr => $token:expr),* ]) => ({
+            let expected = [ $(($token, String::from($span))),* ];
+            assert_eq!(lex($src), expected);
+        })
+    }
+
+    fn lex(src: &str) -> Vec<(TokenKind, String)> {
+        Lexer::new(&EvalContext::new(), "<test>", src)
+            .map(|t| (t.val, format!("{}-{}", t.span.start, t.span.end)))
+            .collect()
+    }
 
     #[test]
     fn test_lex() {
-        assert_eq!(lex(""), []);
-        assert_eq!(lex("0"), []);
+        use parse::TokenKind::*;
+        assert_lex!("" => []);
+        assert_lex!("0" => ["1:1-1:2" => Int(0)]);
     }
 }
